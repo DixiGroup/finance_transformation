0f3f6f9abedfb18c91a354ec64136f9826c7377e
zefmud <paul.myronov@gmail.com>
2018-08-26 15:56:06 +0300

    Логи помилок зберігаються в окремому файлі

--- finance.py
+++ finance.py
@@ -1,5 +1,7 @@
-import common
 import os
+import sys
+import datetime
+import common
 import sheet0
 import sheet1
 import sheet2
@@ -11,6 +13,16 @@ import sheet7
 
 files = os.listdir(common.INPUT_FOLDER)
 files = [f for f in files if f.endswith(".xls")]
+
+if not os.path.exists("finance.log"):
+    f = open("finance.log", "w")
+    f.close()
+f = open("finance.log", "a")
+sys.stdout = f
+sys.stderr = f
+
+print("----------------")
+print(datetime.datetime.now())
 for f in files:
     full_filename = os.path.join(common.INPUT_FOLDER, f)
     with open(common.CURRENT_FILENAME, "w") as cff:
@@ -24,4 +36,6 @@ for f in files:
     sheet5.main()
     sheet6.main()
     sheet7.main()
+print("No errors were caught")
+print("-----------------")
 
